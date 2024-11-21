9c404edc741c356a40e74a57a7d2497a98017c0a
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2024-11-21 10:02:22 +0700

    constant variable

--- SOLIDITY/CROWDFUNDING/crowdfunding.sol
+++ SOLIDITY/CROWDFUNDING/crowdfunding.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract CrowdFunding {
-     uint256 totalFunds;
+     uint256 public constant MINIMUM_FUND = 0.001 ether;
 
      function fund() public payable {
           uint256 amount = msg.value;
-          totalFunds += amount;
+          require(amount>= MINIMUM_FUND, "Minimum amount is 0.001 ether");
      }
 
      function withdraw() public {
