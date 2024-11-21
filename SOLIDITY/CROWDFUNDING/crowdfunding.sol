825bf85a7fbfe0ef5c3c5ed3da322bd9d4d806f0
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2024-11-21 10:07:51 +0700

    immutable variable

--- SOLIDITY/CROWDFUNDING/crowdfunding.sol
+++ SOLIDITY/CROWDFUNDING/crowdfunding.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.0;
 
 contract CrowdFunding {
      uint256 public constant MINIMUM_FUND = 0.001 ether;
+     address public immutable owner;
+
+     constructor() {
+          owner = msg.sender;
+     }
 
      function fund() public payable {
           uint256 amount = msg.value;
@@ -10,6 +15,7 @@ contract CrowdFunding {
      }
 
      function withdraw() public {
-
+          require(msg.sender == owner, "Only owner can withdraw");
+          payable(msg.sender).transfer(address(this).balance);
      }
 }
