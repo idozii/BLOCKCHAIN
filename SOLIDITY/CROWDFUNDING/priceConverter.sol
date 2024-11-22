a6e787e059070ef604b51263e1b159bab89030cd
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2024-11-22 10:12:25 +0700

    fixed price

--- SOLIDITY/CROWDFUNDING/priceConverter.sol
+++ SOLIDITY/CROWDFUNDING/priceConverter.sol
@@ -5,13 +5,14 @@ import {AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/A
 
 library PriceConverter {
      function getPrice() internal view returns (uint256) {
-          AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+          AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
           (,int price,,,) = priceFeed.latestRoundData();
-          return uint256(price);
+          require(price > 0, "Invalid price");
+          return uint256(price) / 1e8;
      }
      function getConversionRate(uint256 amount) internal view returns (uint256) {
           uint256 ethPrice = getPrice();
-          uint256 ethAmountInUSD = (amount * ethPrice) / 1e8;
+          uint256 ethAmountInUSD = (amount * ethPrice) / 1e18;
           return ethAmountInUSD;
      }
 }
